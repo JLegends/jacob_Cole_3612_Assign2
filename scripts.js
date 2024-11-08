7e3e4304caedac90a67d2de40f778d3d4dd4eb02
JLegends <113209667+JLegends@users.noreply.github.com>
2024-11-07 21:20:27 -0700

    cleaned up the code

--- scripts.js
+++ scripts.js
@@ -1,124 +1,35 @@
-document.addEventListener('DOMContentLoaded', () => loadView("home"));
+document.addEventListener('DOMContentLoaded', () => loadView("home")); 
 
 function loadView(view, season = null) {
     const container = document.querySelector("#container");
 
 
     if (view === "home") {
-
         showNavButtons(false);
-
-        container.innerHTML = `        
-            <div id = "content-area">
-                <p> what this site is about blah blah box lorem upsum more words just to 
-                    have an accurate representation more words more words just as an example
-                    paragraph describing what this website does 
-                </p>
-                <div id = "select_szn">
-                <h3> Season </h3>
-                <select id = "season-select" > 
-                    <option> Select season </option>
-                    <option value=2020> 2020 </option>
-                    <option value=2021> 2021 </option>
-                    <option value=2022> 2022 </option>
-                    <option value=2023> 2023 </option>
-                </select>
-                </div>
-            </div>
-            <img src="data/images/raceCarPhoto.webp" id="car_img"> <!-- Add a race car photo here -->`;
-
-            const seasonSelect = document.querySelector("#season-select");
-            seasonSelect.addEventListener("change", (e) => {
-                const selectedSeason = e.target.value;
-                if (selectedSeason) {
-                    loadView("races", selectedSeason);
-                }
-            })
+        loadHomeView(container);
     }
 
     if (view === "races") {
         container.innerHTML = ``; /* This clears the home view */
+
         showNavButtons(true);
 
-        const raceArray = [{year: 2020, round: 1, name:"Italian Grand Prix"}, {year: 2021, round: 1, name:"British Grand Prix"}];
+        /* Just a testing array to see if things are working properly */
+        const racesArray = [{year: 2020, round: 1, name:"Italian Grand Prix"}, 
+            {year: 2021, round: 1, name:"British Grand Prix"}, 
+            {year: 2021, round: 2, name:"German Grand Prix"},
+            {year: 2021, round: 3, name:"France Grand Prix"}];
 
-        const races_container = document.createElement("div");
-        races_container.id = "races_container";
-
-        const titles = document.createElement("h3");
-        titles.className = "titles";
-        titles.textContent = `${season} Races`;
-
-        const table = document.createElement("table");
-        table.id = "races";
-
-        const headerRow = document.createElement("tr");
-
-        const roundColumn = document.createElement("th");
-        roundColumn.textContent = "Round";
-
-        const nameColumn = document.createElement("th");
-        nameColumn.textContent = "Name";
-
-        const resultColumn = document.createElement("th");
-        nameColumn.textContent = "Results";
-
-
-        headerRow.appendChild(roundColumn);
-        headerRow.appendChild(nameColumn);
-        headerRow.appendChild(resultColumn);
-
-
-        table.appendChild(headerRow);
-
-        for (let race of raceArray) {
-            if (race.year == season) {
-                const row = document.createElement("tr");
-
-                const round = document.createElement("td");
-                const name = document.createElement("td");
-                /* needed for button later */
-                const results = document.createElement("td");
-
-                round.textContent = race.round;
-                name.textContent = race.name;
-                results.textContent = "View Results";
-
-                row.appendChild(round);
-                row.appendChild(name);
-                row.appendChild(results);
-
-                table.appendChild(row);
-
-            }
-        }
-        races_container.appendChild(titles);
-        races_container.appendChild(table);
-
-        container.appendChild(races_container);
-
-        /* Now the second div */
-        const results_container = document.createElement("div");
-        results_container.id = "results_container";
-
-        const resultTitle = document.createElement("h3");
-        resultTitle.className = "titles";
-        resultTitle.textContent = `Results for ${season} Italian Grand Prix`;
-
-        description = document.createElement("p");
-        description.textContent = "Race Name, Round #, year, Circuit Name, Date URL (clean this up later)";
-
-        const results = document.createElement("div");
-        results.id = "results";
-
-        results_container.appendChild(resultTitle);
-        results_container.appendChild(description);
-        results_container.appendChild(results);
-
-        container.appendChild(results_container);
+        list_season_races(season, racesArray);
+        list_grandprix_results(container, season, racesArray);
     }
 }
 
+
+/*------------------------------------------------------------------------------------------------------*/
+// Name: showNavButtons
+// Purpose: shows and hides the navigation buttons depending on the current view
+/*------------------------------------------------------------------------------------------------------*/
 function showNavButtons(show) {
     nav_buttons = ["Home", "Favorites"];
     const header = document.querySelector("header");
@@ -145,4 +56,141 @@ function showNavButtons(show) {
             }
         }
     }
+}
+
+/*--------------------------------------------------------------------------------------------------------
+// Name: loadHomeView
+// Purpose: generate the innerHTML and grab the season from user selection
+/*------------------------------------------------------------------------------------------------------*/
+function loadHomeView() {
+    container.styles = `border: solid black;`;
+    container.innerHTML = `        
+    <div id = "content-area">
+        <p> what this site is about blah blah box lorem upsum more words just to 
+            have an accurate representation more words more words just as an example
+            paragraph describing what this website does 
+        </p>
+        <div id = "select_szn">
+        <h3> Season </h3>
+        <select id = "season-select" > 
+            <option> Select season </option>
+            <option value=2020> 2020 </option>
+            <option value=2021> 2021 </option>
+            <option value=2022> 2022 </option>
+            <option value=2023> 2023 </option>
+        </select>
+        </div>
+    </div>
+    <img src="data/images/raceCarPhoto.webp" id="car_img"> <!-- Add a race car photo here -->`;
+
+    const seasonSelect = document.querySelector("#season-select");
+    seasonSelect.addEventListener("change", (e) => {
+        const selectedSeason = e.target.value;
+        if (selectedSeason) {
+            loadView("races", selectedSeason);
+        }
+    });
+}
+
+/*--------------------------------------------------------------------------------------------------------
+// Name: list_season_races
+// Purpose: it creates the DOM elements for the season races block
+/*------------------------------------------------------------------------------------------------------*/
+function list_season_races(season, racesArray) {
+    container.styles.container = `border: none;`;
+
+    const races_container = document.createElement("div");
+    races_container.id = "races_container";
+
+    const titles = document.createElement("h3");
+    titles.className = "titles";
+    titles.textContent = `${season} Races`;
+
+    const round_container = document.createElement("div");
+    round_container.id = "round_container";
+
+    const table = document.createElement("table");
+    table.id = "races";
+
+    const headerRow = document.createElement("tr");
+
+    const roundColumn = document.createElement("th");
+    roundColumn.textContent = "Round";
+
+    const nameColumn = document.createElement("th");
+    nameColumn.textContent = "Name";
+
+    headerRow.appendChild(roundColumn);
+    headerRow.appendChild(nameColumn);
+
+    round_container.appendChild(headerRow)
+
+    table.appendChild(round_container);
+
+    generate_rounds_table(round_container, table, season, racesArray);
+
+    races_container.appendChild(titles);
+    races_container.appendChild(table);
+
+    container.appendChild(races_container);
+}
+
+/*--------------------------------------------------------------------------------------------------------
+// Name: generate_rounds_table
+// Purpose: generates the table of rounds that took place in a season
+/*------------------------------------------------------------------------------------------------------*/
+function generate_rounds_table(round_container, table, season, racesArray) {
+    for (let race of racesArray) {
+        if (race.year == season) {
+            const row = document.createElement("tr");
+            row.className = "round_rows";
+
+            const round = document.createElement("td");
+            const name = document.createElement("td");
+            /* needed for button later */
+            const results = document.createElement("td");
+            const resultsButton = document.createElement("button");
+            results.class = "";
+
+            round.textContent = race.round;
+            name.textContent = race.name;
+            resultsButton.textContent = "Results";
+
+
+            row.appendChild(round);
+            row.appendChild(name);
+
+            results.appendChild(resultsButton);
+            row.appendChild(results);
+
+            round_container.appendChild(row)
+
+            table.appendChild(round_container);
+        }
+    }
+}
+
+/*--------------------------------------------------------------------------------------------------------
+// Name: list_grandprix_results
+// Purpose: creates the DOM content for the selection grand prix results
+/*------------------------------------------------------------------------------------------------------*/
+function list_grandprix_results(container, season, racesArray) {
+    const results_container = document.createElement("div");
+    results_container.id = "results_container";
+
+    const resultTitle = document.createElement("h3");
+    resultTitle.className = "titles";
+    resultTitle.textContent = `Results for ${season} Italian Grand Prix`;
+
+    description = document.createElement("p");
+    description.textContent = "Race Name, Round #, year, Circuit Name, Date URL (clean this up later)";
+
+    const results = document.createElement("div");
+    results.id = "results";
+
+    results_container.appendChild(resultTitle);
+    results_container.appendChild(description);
+    results_container.appendChild(results);
+
+    container.appendChild(results_container);
 }
